d27c4006dbd10991c568346e2d624b61e11b3250
Adamizgoi <adamizgoi97@gmail.com>
2022-09-17 01:34:50 +0300

    Bugfix for first package of unit tests PlayerTest

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -12,30 +12,29 @@ public class PlayerTest {
 
     /** Блок тестов к методу installGame
      */
-    /* @Test
-    public void shouldAddGameIfGameIsNew() {
-        Player crasher = new Player("crasher");
-        crasher.installGame(game1);
-
-        Game expected = game1;
-        Game actual =
-        Тут нужно проверить через mostPlayerByGenre, но этот метод не работает
-    }
-
-    @Test
-    public void shouldNotAddGameIfGameIsAlreadySaved()
-    */
+   /* @Test
+   public void shouldAddGameIfGameIsNew() {
+       Player crasher = new Player("crasher");
+       crasher.installGame(game1);
+       Game expected = game1;
+       Game actual =
+       Тут нужно проверить через mostPlayerByGenre, но этот метод не работает
+   }
+   @Test
+   public void shouldNotAddGameIfGameIsAlreadySaved()
+   */
     /** Конец блока тестов к методу installGame
      */
 
 
-    /** Блок тестов к методу play
+    /**
+     * Блок тестов к методу play
      */
     @Test
     public void shouldAddPlayTimeToGameStore() {
         Player crasher = new Player("crasher");
         crasher.installGame(game1);
-        crasher.play(game1,5);
+        crasher.play(game1, 5);
 
         int expected = 5;
         int actual = store.getSumPlayedTime();
@@ -47,7 +46,7 @@ public class PlayerTest {
     public void shouldAddPlayTimeToGameStoreIfPlayTimeIsZero() {
         Player crasher = new Player("crasher");
         crasher.installGame(game1);
-        crasher.play(game1,0);
+        crasher.play(game1, 0);
 
         int expected = 0;
         int actual = store.getSumPlayedTime();
@@ -59,7 +58,7 @@ public class PlayerTest {
     public void shouldNotAddPlayTimeToGameStoreIfPlayTimeIsLessZero() {
         Player crasher = new Player("crasher");
         crasher.installGame(game1);
-        crasher.play(game1,-1);
+        crasher.play(game1, -1);
 
         int expected = 0;
         int actual = store.getSumPlayedTime();
@@ -71,20 +70,35 @@ public class PlayerTest {
     public void shouldAddPlayTimeToPlayerMapIfPlayTimeIsZero() {
         Player crasher = new Player("crasher");
         crasher.installGame(game1);
-        crasher.play(game1,0);
 
         int expected = 0;
-        int actual = store.getSumPlayedTime();
+        int actual = crasher.play(game1, 0);
 
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+    public void shouldAddPlayTimeToPlayerMap() {
+        Player crasher = new Player("crasher");
+        crasher.installGame(game1);
+
+        int expected = 5;
+        int actual = crasher.play(game1, 5);
+        ;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    /**
+     * ТУТ ВАЖНО ПОПРАВИТЬ МЕТОД В ФР
+     */
+    @Test
     public void shouldAddPlayTimeToPlayerMapIfUserPlayedSeveralTimes() {
         Player crasher = new Player("crasher");
-        crasher.play(game1,5);
-        crasher.play(game1,10);
-        crasher.play(game1,1);
+        crasher.installGame(game1);
+        crasher.play(game1, 5);
+        crasher.play(game1, 10);
+        crasher.play(game1, 1);
 
         int expected = 16;
         int actual = store.getSumPlayedTime();
@@ -95,17 +109,19 @@ public class PlayerTest {
     @Test
     public void shouldThrowRuntimeExceptionIfUserTryAddPlayTimeInNotInstalledGame() {
         Player crasher = new Player("crasher");
-        crasher.play(game1error,5);
 
-        Assertions.assertThrows(RuntimeException.class, () -> {});
+        Assertions.assertThrows(RuntimeException.class, () -> {
+                    crasher.play(game1error, 5);
+                }
+        );
     }
 
     /** Конец блока тестов к методу play
      */
 
 
-
-    /** Блок тестов к методу sumGenre
+    /**
+     * Блок тестов к методу sumGenre
      */
     @Test
     public void shouldSumGenreIfOneGame() {
@@ -147,20 +163,18 @@ public class PlayerTest {
      */
 
 
-
     /** Блок тестов к методу mostPlayerByGenre
      */
-        /*@Test
-    Если юзер установил игру, но не играл play в нее, то выдается сумма.. ноль?
-    public void shouldNotConsistPlayTimeIfUserInstalGameButDidNotPlay() {
-        Player crasher = new Player("crasher");
-        crasher.installGame(game1);
-
-        int expected = ???;
-        int actual = store.getSumPlayedTime();
-
-        Assertions.assertEquals(expected, actual);
-    }*/
+       /*@Test
+   Если юзер установил игру, но не играл play в нее, то выдается сумма.. ноль?
+   public void shouldNotConsistPlayTimeIfUserInstalGameButDidNotPlay() {
+       Player crasher = new Player("crasher");
+       crasher.installGame(game1);
+       int expected = ???;
+       int actual = store.getSumPlayedTime();
+       Assertions.assertEquals(expected, actual);
+   }*/
     /** Конец блока тестов к методу mostPlayerByGenre
      */
 }
+
